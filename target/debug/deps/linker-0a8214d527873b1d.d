91b16cc73224a2ef750c5989f2a7396e28fd5a92
shiwenlong12 <1300160147@qq.com>
2023-04-27 08:09:49 +0800

    4.27

---
+++ target/debug/deps/linker-0a8214d527873b1d.d
@@ -0,0 +1,8 @@
+/home/swl/os/crate_module/linker/target/debug/deps/linker-0a8214d527873b1d.rmeta: src/lib.rs src/app.rs
+
+/home/swl/os/crate_module/linker/target/debug/deps/liblinker-0a8214d527873b1d.rlib: src/lib.rs src/app.rs
+
+/home/swl/os/crate_module/linker/target/debug/deps/linker-0a8214d527873b1d.d: src/lib.rs src/app.rs
+
+src/lib.rs:
+src/app.rs:
---
+++ target/debug/liblinker.d
@@ -0,0 +1,1 @@
+/home/swl/os/crate_module/linker/target/debug/liblinker.rlib: /home/swl/os/crate_module/linker/src/app.rs /home/swl/os/crate_module/linker/src/lib.rs
